837d8e3bde6847c1187ae19ddcd5c2882d00366e
Gia Viet <vietxuyen97@gmail.com>
2018-12-30 17:12:56 +0700

    hieu ung js cho fb

--- hieu-ung-js/hieu-ung-click-fb/main.js
+++ hieu-ung-js/hieu-ung-click-fb/main.js
@@ -1,16 +1,29 @@
-window.onload = function(){
-    const click = document.querySelector('.tamgiac');
-    const list = document.querySelector('.danhsach');
-    var i=0;
+window.onload = () => {
+    const click = document.querySelectorAll('.icon');
+    for (let i = 0; i < click.length; i++) {
+        click[i].onclick = function(e){
 
-    click.addEventListener('click', (e) => {
-        if(i%2==0){
-            list.style.display = "initial";
-            click.style.color = "white";
-        }else{
-            list.style.display = "none";
-            click.style.color = "black";
+            this.classList.toggle('mautrang');
+            const b = e.target.parentNode;
+            for(let j=0;j<click.length;j++){
+                if(click[j] !== b){
+                    click[j].classList.remove('mautrang');
+                }
+                
+            }
+          
+            const danhsach = document.querySelectorAll('.danhsach');
+          
+            const hien = document.getElementById(this.getAttribute('data-click'));
+            hien.classList.toggle('an');
+            const a = e.target.parentNode.nextElementSibling;
+            for(let k=0;k<danhsach.length;k++){
+                if(danhsach[k] !== a){
+                    danhsach[k].classList.remove('an');
+                }
+            }
         }
-        i++;
-    });
+        
+    }
+    
 }
